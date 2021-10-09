6f433badefebcfdbb170c7da7ad26f8a1e4926da
Satya-Chandana <msatyachandana@gmail.com>
2021-10-10 01:29:15 +0530

    expense

---
+++ expense/urls.py
@@ -0,0 +1,35 @@
+"""website URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include, re_path
+from django.shortcuts import reverse
+from django.http import HttpResponseRedirect
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(('accounts.urls', 'accounts'), namespace='accounts')),
+    path('', include(('expense_manager.urls', 'expense_manager'),
+                     namespace='expense_manager')),
+    re_path(r'^$', lambda r: HttpResponseRedirect(reverse('accounts:login'))),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
+
